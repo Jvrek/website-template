d88aa547bd0f4920c2e83557f37897b7741ef278
Jaroslaw Spyrka <jaroslaw.spyrka@gmail.com>
2024-01-15 23:52:34 +0100

    feat: pre-coomit pretier + eslint

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,14 +1,14 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
 
 module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
+  mode: "development",
+  devtool: "inline-source-map",
   devServer: {
     liveReload: true,
-    port: '9099',
+    port: "9099",
     hot: true,
     open: true,
-    static: ['./'],
+    static: ["./"],
   },
 });
