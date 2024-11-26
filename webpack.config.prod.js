69dc6e4251090bd765404e99d26acdf9c056c33f
Jarosław Spyrka <spyrka.jarek@gmail.com>
2024-11-26 21:53:43 +0100

    chore: base working webpack version

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,15 +4,53 @@ const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
 const common = require('./webpack.common');
+const path = require('path');
+
+// Lista dodatkowych plików HTML (oprócz index.html)
+const htmlFiles = [
+  '404.html',
+  'contact-options.html',
+  'kosmetologia.html',
+  'fryzjerstwo.html',
+  'newsletter.html',
+  'newsletter-success-page.html',
+  'privacy-policy.html',
+];
+
+// Tworzenie instancji HtmlWebpackPlugin dla każdego dodatkowego pliku HTML
+const htmlPlugins = htmlFiles.map((file) => {
+  return new HtmlWebpackPlugin({
+    filename: file, // Wyjściowy plik w katalogu 'dist'
+    template: `./${file}`, // Ścieżka do pliku źródłowego
+    inject: 'body', // Wstawianie zasobów (CSS/JS) w <body>
+    minify: {
+      removeComments: true,
+      collapseWhitespace: true,
+    },
+  });
+});
 
 module.exports = merge(common, {
   mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].[contenthash].js', // Wersjonowanie JS
+    clean: true, // Usunięcie starych plików przy każdym buildzie
+  },
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].css',
-    }),
+    // Tworzenie index.html
     new HtmlWebpackPlugin({
-      template: './index.html',
+      filename: 'index.html', // Główny plik index.html
+      template: './index.html', // Ścieżka do oryginalnego index.html
+      inject: 'body', // Wstawianie zasobów (CSS/JS) w <body>
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+      },
+    }),
+    ...htmlPlugins, // Dynamiczne generowanie innych plików HTML
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash].css', // Wersjonowanie CSS
     }),
     new webpack.ProvidePlugin({
       $: 'jquery',
@@ -21,22 +59,12 @@ module.exports = merge(common, {
     }),
     new CopyPlugin({
       patterns: [
-        { from: 'assets', to: 'assets' },
+        { from: 'assets', to: 'assets' }, // Kopiowanie folderów
         { from: 'js/vendor', to: 'js/vendor' },
         { from: 'icon.svg', to: 'icon.svg' },
         { from: 'favicon.ico', to: 'favicon.ico' },
         { from: 'robots.txt', to: 'robots.txt' },
         { from: 'icon.png', to: 'icon.png' },
-        { from: '404.html', to: '404.html' },
-        { from: 'contact-options.html', to: 'contact-options.html' },
-        { from: 'kosmetologia.html', to: 'kosmetologia.html' },
-        { from: 'fryzjerstwo.html', to: 'fryzjerstwo.html' },
-        { from: 'newsletter.html', to: 'newsletter.html' },
-        {
-          from: 'newsletter-success-page.html',
-          to: 'newsletter-success-page.html',
-        },
-        { from: 'privacy-policy.html', to: 'privacy-policy.html' },
         { from: 'site.webmanifest', to: 'site.webmanifest' },
       ],
     }),
@@ -45,11 +73,11 @@ module.exports = merge(common, {
     rules: [
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'], // Produkcyjne CSS
       },
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'], // Produkcyjne SCSS
       },
     ],
   },
